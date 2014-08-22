2f799fc9f5f03e92f46070ea4c7501a2adefd6f0
Pedro <zepedro.correia@gmail.com>
2014-08-22 14:08:26 +0200

    Plotting languages in B&W. Function to process results is parameterizable.

--- data-analysis.R
+++ data-analysis.R
@@ -22,11 +22,11 @@ plot.language <- function(speaker.filename, hearer.filename) {
     melted.speaker$State <- states
     melted.hearer$State <- states
     
-    speaker.plot <- ggplot(melted.speaker, aes(x=State, y=value, colour=variable)) +
-        geom_line() + labs(title='Speaker',x='State',y='') +
+    speaker.plot <- ggplot(melted.speaker, aes(x=State, y=value)) +
+        geom_line(aes(linetype=variable)) + labs(title='Speaker',x='State',y='') +
         theme(legend.position = 'none') + ylim(0,1)
-    hearer.plot <- ggplot(melted.hearer, aes(x=State, y=value, colour=variable)) +
-        geom_line() + labs(title='Hearer',x='State',y='') + 
+    hearer.plot <- ggplot(melted.hearer, aes(x=State, y=value)) +
+        geom_line(aes(linetype=variable)) + labs(title='Hearer',x='State',y='') + 
         theme(legend.position = 'none')
     
     return(arrangeGrob(speaker.plot, hearer.plot))
@@ -43,24 +43,24 @@ plot.cluster <- function(language.names, data) {
     
 }
 
-do.stuff <- function() {
-    x <- read.csv('results/20140121-141158.csv', colClasses=c('numeric','factor','numeric','numeric','numeric','factor','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','character','character'))
+do.stuff <- function(timestamp) {
+    x <<- read.csv(paste(sep='','results/', timestamp,'.csv'), colClasses=c('numeric','factor','numeric','numeric','numeric','factor','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric','character','character'))
     width <- 8
     height <- 5
     ggplot(x, aes(x=factor(Impairment), y=Speaker.entropy)) + geom_boxplot() + xlab('Impairment') + ylim(0, 1) + ylab(expression(E(sigma)))
-    ggsave(filename=paste(sep='', 'paper/plots/', 'Speaker-entropy-', '20140121-141158.png'), width=width, height=height)
+    ggsave(filename=paste(sep='', 'paper/plots/', 'Speaker-entropy-', timestamp, '.png'), width=width, height=height)
     ggplot(x, aes(x=factor(Impairment), y=Hearer.entropy)) + geom_boxplot() + xlab('Impairment') + ylim(0, 1) + ylab(expression(E(rho)))
-    ggsave(filename=paste(sep='', 'paper/plots/', 'Hearer-entropy-', '20140121-141158.png'), width=width, height=height)
+    ggsave(filename=paste(sep='', 'paper/plots/', 'Hearer-entropy-', timestamp, '.png'), width=width, height=height)
     ggplot(x, aes(x=factor(Impairment), y=Speaker.informativity)) + geom_boxplot() + xlab('Impairment') + ylim(0, 1) + ylab(expression(I(sigma)))
-    ggsave(filename=paste(sep='', 'paper/plots/', 'Speaker-informativity-', '20140121-141158.png'), width=width, height=height)
+    ggsave(filename=paste(sep='', 'paper/plots/', 'Speaker-informativity-', timestamp, '.png'), width=width, height=height)
     ggplot(x, aes(x=factor(Impairment), y=Hearer.informativity)) + geom_boxplot() + xlab('Impairment') + ylim(0, 1) + ylab(expression(I(rho)))
-    ggsave(filename=paste(sep='', 'paper/plots/', 'Hearer-informativity-', '20140121-141158.png'), width=width, height=height)
+    ggsave(filename=paste(sep='', 'paper/plots/', 'Hearer-informativity-', timestamp, '.png'), width=width, height=height)
     ggplot(x, aes(x=factor(Impairment), y=Speaker.Voronoiness)) + geom_boxplot() + xlab('Impairment') + ylim(0, 1) + ylab(expression(V(sigma)))
-    ggsave(filename=paste(sep='', 'paper/plots/', 'Speaker-Voronoiness-', '20140121-141158.png'), width=width, height=height)
+    ggsave(filename=paste(sep='', 'paper/plots/', 'Speaker-Voronoiness-', timestamp, '.png'), width=width, height=height)
     ggplot(x, aes(x=factor(Impairment), y=Hearer.Voronoiness)) + geom_boxplot() + xlab('Impairment') + ylim(0, 1) + ylab(expression(V(rho)))
-    ggsave(filename=paste(sep='', 'paper/plots/', 'Hearer-Voronoiness-', '20140121-141158.png'), width=width, height=height)
+    ggsave(filename=paste(sep='', 'paper/plots/', 'Hearer-Voronoiness-', timestamp, '.png'), width=width, height=height)
     ggplot(x, aes(x=factor(Impairment), y=Expected.utility)) + geom_boxplot() + xlab('Impairment') + ylim(0, 1) + ylab(expression(EU(sigma,rho)))
-    ggsave(filename=paste(sep='', 'paper/plots/', 'Expected-utility-', '20140121-141158.png'), width=width, height=height)
+    ggsave(filename=paste(sep='', 'paper/plots/', 'Expected-utility-', timestamp, '.png'), width=width, height=height)
     ggplot(x, aes(x=factor(Impairment), y=Iterations)) + geom_boxplot() + xlab('Impairment')
-    ggsave(filename=paste(sep='', 'paper/plots/', 'Iterations-', '20140121-141158.png'), width=width, height=height)
+    ggsave(filename=paste(sep='', 'paper/plots/', 'Iterations-', timestamp, '.png'), width=width, height=height)
 }
