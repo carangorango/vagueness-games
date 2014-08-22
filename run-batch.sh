7ef07e84e8f9e4d3341a95749c82cbc92d19748c
Pedro <zepedro.correia@gmail.com>
2014-08-22 14:12:50 +0200

    Generated new set of results (100 per parameter combination)

--- run-batch.sh
+++ run-batch.sh
@@ -4,16 +4,17 @@ DATE=$(date +%Y%m%d-%H%M%S)
 HEADER="Number of states,Prior distribution type,Number of messages,Impairment,Tolerance,Dynamics,Speaker entropy,Hearer entropy,Speaker convexity,Hearer convexity,Speaker Voronoiness,Hearer Voronoiness,Speaker informativity,Hearer informativity,Expected utility,Iterations,Speaker strategy file,Hearer strategy file"
 
 echo $HEADER > results/$DATE.csv
-for t in 6 10 50 90
+for t in 10 50 90 130
 do
     echo $t
-    for i in 0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4
+    for i in 0 0.1 0.2 0.3 0.4 0.5
     do
         echo " $i"
-    	for n in {1..10}
+    	for n in {1..100}
     	do
-	        echo "  $n"
+	        echo -n "  $n"
         	python vagueness-games.py $t 2 $i results/$DATE.csv
     	done
+    	echo
     done
 done
