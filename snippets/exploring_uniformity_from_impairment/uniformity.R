b78c39e06aea79977418c924564e9fd1eccf8790
Michael Franke <the.secret.goldfish@me.com>
2014-09-23 17:11:26 +0200

    in group EU

--- snippets/exploring_uniformity_from_impairment/uniformity.R
+++ snippets/exploring_uniformity_from_impairment/uniformity.R
@@ -1,3 +1,5 @@
+computeD == FALSE
+
 data = read.csv('~/Desktop/data/svn/vagueness-games/results/20140918-142408.varTolerance.csv', 
                 colClasses=c('numeric','factor','numeric','numeric','numeric',
                              'factor','numeric','numeric','numeric','numeric',
@@ -16,14 +18,11 @@ plot.language.MinMaxProperty = function(dataFrame,colindex){
                   plot.language.byindex(dataFrame,which.max(dataFrame[,colindex])))
 }
 
-
-
 # Hellinger distance.
 
 HD = function(x, y){
   return( sqrt(sum(sapply(1:length(x), function(i) (sqrt(x[i]) - sqrt(y[i]))^2  )) ) / (sqrt(2)) )
 }
-
 
 # Distance between two strategis as Hellinger distances at each choice point.
 
@@ -116,5 +115,64 @@ ds = data[,c(1,4,5,19)] # only relevant "data" is speaker strategy file name
 # ds = subset(ds,Number.of.states == 50 & Impairment == 0 & Tolerance <= 0.3)
 dm = melt(ds,id.vars = c("Number.of.states", "Impairment", "Tolerance")) # melted data
 
-D = ddply(dm, .(Number.of.states, Impairment, Tolerance), summarise, 
+if (computeD == TRUE){
+  D = ddply(dm, .(Number.of.states, Impairment, Tolerance), summarise, 
             inGroupDistance = speaker.vector.distance(value))
+}
+else{
+  D = read.csv("~/Desktop/data/svn/vagueness-games/snippets/exploring_uniformity_from_impairment/D.csv")
+}
+
+pd <- position_dodge(.01)
+sp = ggplot(D, aes(x=Impairment, y=inGroupDistance)) + 
+  geom_point(position = pd) + geom_line()
+sp + facet_grid(Number.of.states ~ Tolerance, scales="free")
+
+# compute average EU between different strategies from the same parameter triple
+
+get.confusion.matrix = function(states,alpha){
+  ns = length(states)
+  if (alpha <= 0){
+    return(diag(rep(1,ns)))
+  }
+  C = matrix(0,nrow=ns, ncol=ns)
+  for (i in 1:ns){
+    for (j in 1:ns){
+      C[i,j] = exp( - (states[i]-states[j])^2 / (alpha^2) )
+    }
+  }
+  return(C)
+}
+
+expected.utility = function(S,H,U,states) {
+  # assumes that priors are flat
+  return(1/ns * (sum(sapply(1:ns, function(t) sum(sapply(1:2, function(m) sum(sapply(1:ns, function(t2) S[t,m]*H[m,t]*U[t,t2])) )))))) 
+}
+
+expected.utility.byIndeces = function(data, i, j){
+  # i is sender strat index, j is hearer strat index
+  ns = data[i,]$Number.of.states
+  states = seq(0,1,length.out = ns)
+  tol = data[i,]$Tolerance
+  U = get.confusion.matrix(states,tol)
+  S = read.speaker.strategy(dataFrame = data, rowIndex = i)
+  H = read.hearer.strategy(dataFrame = data, rowIndex = j)
+  eu1 = expected.utility(S,H,U,states)
+  eu2 = expected.utility(S,H[c(2,1),],U,states)
+  return(max(eu1,eu2))
+}
+
+i = 1
+j = 2
+show(arrangeGrob(plot.language.byindex(data,i),plot.language.byindex(data,j)))
+show(expected.utility.byIndeces(data, i, j))
+
+data$rowIndex = 1:nrow(data)
+data$meanEUinGroup = rep(0,nrow(data))
+
+for (i in 3501:3550){
+  dsub = subset(data, Impairment == data[i,]$Impairment &
+                Tolerance == data[i,]$Tolerance &
+                Number.of.states == data[i,]$Number.of.states)
+  data[i,]$meanEUinGroup = mean(sapply(dsub$rowIndex, function(j) expected.utility.byIndeces(data,i, j)))
+}
